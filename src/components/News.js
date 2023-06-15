05671b7d3fa9d12b1ec78c71ea71727bc8554f80
GENTLEW1ND <rajchakraborty029@gmail.com>
2023-06-15 15:14:33 +0530

    used api

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news Component
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>HashtagNews-Top headlines</h2>
+        <div className='row'>
+          <div className='col-md-4'>
+        <NewsItem title = "my title" description = "my des"/>
+        </div>
+          <div className='col-md-4'>
+        <NewsItem title = "my title" description = "my des"/>
+        </div>
+          <div className='col-md-4'>
+        <NewsItem title = "my title" description = "my des"/>
+        </div>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props;
     return (
       <div>
-        This is an item
+        <div className="card" style={{width: "18rem;"}}>
+          <img src="https://content.api.news/v3/images/bin/a2a371f14b3ec9c965ac0ba685307a1c" className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
       </div>
     )
   }
